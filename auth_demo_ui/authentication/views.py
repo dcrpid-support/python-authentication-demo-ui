2bd2d2552c696435f4d94f48e01c08b7eae16781
John Benedict Ty <j.ty@psa.gov.ph>
2025-08-20 15:40:44 +0800

    Optimized API for authentication version 3

--- auth_demo_ui/authentication/views.py
+++ auth_demo_ui/authentication/views.py
@@ -118,7 +118,7 @@ def authenticate(request):
     base_url = os.environ.get('BASE_URL')
     version = os.environ.get('VERSION')
 
-    value['input_ekyc'] = value['input_ekyc'] in ['1', 'true', 't', 'yes', 'y']
+    value['input_ekyc'] = value['input_ekyc'] in ['1', 'true', 't', 'yes', 'y'] or value['input_ekyc']
     
     auth_url = base_url + '/idauthentication/v1/' + ('kyc/' if value['input_ekyc'] else 'auth/') + os.environ.get('TSP_LICENSE_KEY') + '/' + os.environ.get('PARTNER_ID') + '/' + os.environ.get('API_KEY')
     datetime_now = get_current_time()
